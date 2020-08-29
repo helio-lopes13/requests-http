72841b95e755e8d7cd722aec0f6f30c496825732
helio-lopes13 <heliosgod13@hotmail.com>
2020-08-29 13:10:31 -0300

    Adição de serviço para conseguir os cursos da API.

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from '../cursos.service';
+import { Curso } from "../models/curso.model";
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  cursos: Curso[];
+
+  constructor(
+    private service: CursosService
+  ) { }
 
   ngOnInit(): void {
+    this.service.list().subscribe(
+      (dados) => (this.cursos = dados)
+    );
   }
 
 }
