b7a4ef69078941f5f7e34e067737caebfa3b82ea
helio-lopes13 <heliosgod13@hotmail.com>
2020-08-31 12:03:28 -0300

    Modificação de controle de erros. Controle de erros pelo template, pelo console e alerta usando um modal.

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from "../models/curso.model";
+import { Observable, empty, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { AlertModalComponent } from '../../shared/alert-modal/alert-modal.component';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -10,16 +14,54 @@ import { Curso } from "../models/curso.model";
 })
 export class CursosListaComponent implements OnInit {
 
-  cursos: Curso[];
+  // cursos: Curso[];
+  cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
+  bsModalRef: BsModalRef;
 
   constructor(
-    private service: CursosService
+    private service: CursosService,
+    private modalService: BsModalService
   ) { }
 
   ngOnInit(): void {
-    this.service.list().subscribe(
-      (dados) => (this.cursos = dados)
+    // this.service.list().subscribe(
+    //   (dados) => (this.cursos = dados)
+    // );
+
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(
+        (err) => {
+          console.error(err);
+          //this.error$.next(true);
+          this.handleError();
+          return empty();
+        }
+      )
     );
+
+    // this.service.list().subscribe(
+    //   dados => {
+    //     console.log(dados);
+    //   },
+    //   error => {
+    //     console.error(error)
+    //   },
+    //   () => {
+    //     console.log('Observable completo.')
+    //   }
+    // );
+  }
+
+  handleError() {
+    this.bsModalRef = this.modalService.show(AlertModalComponent);
+    this.bsModalRef.content.type = 'danger';
+    this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
   }
 
 }
