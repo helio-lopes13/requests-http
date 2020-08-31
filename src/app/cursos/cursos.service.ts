a42d2faef0e8b543ce213634b849c977bace25e5
helio-lopes13 <heliosgod13@hotmail.com>
2020-08-31 12:02:12 -0300

    Módulo sobre unsubscribe do RxJS. Adição de módulos, componentes de prova de conceito e serviço auxiliar.

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Curso } from './models/curso.model';
-import { tap } from "rxjs/operators";
+import { tap, delay } from "rxjs/operators";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
   constructor(
     private http: HttpClient
   ) { }
 
   list() {
-    return this.http.get<Curso[]>(this.API).pipe(tap());
+    return this.http.get<Curso[]>(this.API).pipe(
+      delay(2000),
+      tap(console.log)
+    );
   }
 }
