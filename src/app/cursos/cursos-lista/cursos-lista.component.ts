8af9ae1006d85b6454644fbbdf4d73bf8b108e1c
helio-lopes13 <heliosgod13@hotmail.com>
2020-09-03 15:18:08 -0300

    Adição de serviço de modal de alerta genérico. Adição do serviço e refatoração do código utilizando o modal de alerta.

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -4,7 +4,7 @@ import { Curso } from "../models/curso.model";
 import { Observable, empty, Subject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { AlertModalComponent } from '../../shared/alert-modal/alert-modal.component';
+import { AlertModalService } from '../../shared/alert-modal.service';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -21,7 +21,8 @@ export class CursosListaComponent implements OnInit {
 
   constructor(
     private service: CursosService,
-    private modalService: BsModalService
+    private modalService: BsModalService,
+    private alertService: AlertModalService
   ) { }
 
   ngOnInit(): void {
@@ -59,9 +60,10 @@ export class CursosListaComponent implements OnInit {
   }
 
   handleError() {
-    this.bsModalRef = this.modalService.show(AlertModalComponent);
-    this.bsModalRef.content.type = 'danger';
-    this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
+    this.alertService.showAlertDanger('Erro ao carregar cursos. Tente novamente mais tarde.');
+    // this.bsModalRef = this.modalService.show(AlertModalComponent);
+    // this.bsModalRef.content.type = 'danger';
+    // this.bsModalRef.content.message = 'Erro ao carregar cursos. Tente novamente mais tarde.';
   }
 
 }
