262be7739085d2e39a788af8a0f8a04a524f34b6
helio-lopes13 <heliosgod13@hotmail.com>
2020-09-07 12:18:18 -0300

    Adição de formulário para cursos. Criação de componente para formulário reativo e alteração das rotas. O formulário serve para criar e alterar cursos.

---
+++ src/app/cursos/cursos-form/cursos-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cursos-form',
+  templateUrl: './cursos-form.component.html',
+  styleUrls: ['./cursos-form.component.scss']
+})
+export class CursosFormComponent implements OnInit {
+
+  form: FormGroup;
+  submitted: boolean = false;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      nome: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.form.value);
+    if (this.form.valid) {
+      console.log('Submit');
+    }
+  }
+
+  onCancel() {
+    this.submitted = false;
+    this.form.reset();
+  }
+
+  hasError(field: string) {
+    return this.form.get(field).errors;
+  }
+
+}
